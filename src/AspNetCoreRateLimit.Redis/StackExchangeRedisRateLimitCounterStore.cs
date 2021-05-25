ddb5b0a42eeacb29b658adef79a10820d982b583
Nick Cromwell <nick.cromwell@rangerlabs.io>
2021-05-25 09:02:37 -0400

    Initial commit

---
+++ src/AspNetCoreRateLimit.Redis/StackExchangeRedisRateLimitCounterStore.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+namespace AspNetCoreRateLimit.Redis
+{
+    public class StackExchangeRedisRateLimitCounterStore : StackExchangeRedisRateLimitStore<RateLimitCounter?>, IRateLimitCounterStore
+    {
+        public StackExchangeRedisRateLimitCounterStore(IConnectionMultiplexer connectionMultiplexer)
+            : base(connectionMultiplexer)
+        {
+        }
+    }
+}
---
+++ src/AspNetCoreRateLimit.Redis/StartupExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreRateLimit.Redis
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddStackExchangeRedisStores(this IServiceCollection services)
+        {
+            services.AddSingleton<IIpPolicyStore, StackExchangeRedisIpPolicyStore>();
+            services.AddSingleton<IClientPolicyStore, StackExchangeRedisClientPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, StackExchangeRedisRateLimitCounterStore>();
+            services.AddSingleton<IProcessingStrategy, StackExchangeRedisProcessingStrategy>();
+            return services;
+        }
+    }
+}
